f4b631c00a2c413fee292e0a66776b94944539cc
Mateusz <gorzanski.mateusz@gmail.com>
2018-01-27 00:03:14 +0100

    update settings view

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Setting;
 
 class SettingsController extends Controller
@@ -17,4 +18,11 @@ class SettingsController extends Controller
             ['name' => 'always_open_source_of_article', 'value' =>  0, 'user_id' => $userId]
         );
     }
+
+    /*private function getListOfSettings() {
+        $settings = Setting::select('*')
+                            ->where('user_id', '=', Auth::id())
+                            ->orderBy('id', 'desc')
+                            ->get();
+    }*/
 }
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -11,18 +11,20 @@
             <div class="settings__form-row">
                 <label class="settings__label">@lang('settings.label-select-lang')</label>
                 <select class="settings__select" name="languages">
-                    <option>english</option>
-                    <option>polski</option>
+                    <option value="english">english</option>
+                    <option value="polski">polski</option>
                 </select>
             </div>
 
             <div class="settings__form-row">
                 <label class="settings__label">@lang('settings.label-always-open-source-of-article')</label>
-                <label class="settings__label--description">@lang('settings.label-always-open-source-of-article-description')</label>
                 <input class="settings__checkbox" name="always_open_source_of_article" type="checkbox">
+                <label class="settings__label--description">@lang('settings.label-always-open-source-of-article-description')</label>
             </div>
             {{ csrf_field() }}
-            <button type="submit" class="settings__submit">@lang('settings.form-submit-btn')</button>
+            <div class="settings__form-row">
+                <button type="submit" class="settings__submit">@lang('settings.form-submit-btn')</button>
+            </div>
         </form>
 	</div>
 </div>
