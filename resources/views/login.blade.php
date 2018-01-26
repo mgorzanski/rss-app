7e6950f4bc40305037ab5671037f5bacbee54799
Mateusz <gorzanski.mateusz@gmail.com>
2018-01-26 17:08:34 +0100

    add css naming conventions to login page and make it responsive

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,24 +7,24 @@
         <link href="{{ asset('css/login.css') }}" rel="stylesheet">
     </head>
     <body>
-        <header class="header">
-            <h1 class="header-title">Today</h1>
+        <header class="header-login">
+            <h1 class="header-login__title">Today</h1>
         </header>
-        <main class="main">
-            <section class="login">
-                <h2 class="section-title">@lang('login.login-heading')</h2>
+        <main class="main-login">
+            <section class="main-login__section-login">
+                <h2 class="main-login__section-title">@lang('login.login-heading')</h2>
                 <form action="/login" method="post">
                     <label>@lang('login.login-label')</label>
                     <input type="text" name="name">
                     <label>@lang('login.password-label')</label>
                     <input type="password" name="password">
                     {{ csrf_field() }}
-                    <p class="remember-label"><input type="checkbox" name="remember"> @lang('login.remember')</p>
+                    <p class="form-remember-label"><input type="checkbox" name="remember"> @lang('login.remember')</p>
                     <button type="submit">@lang('login.login-btn')</button>
                 </form>
             </section>
-            <section class="register">
-                <h2 class="section-title">@lang('login.register-heading')</h2>
+            <section class="main-login__section-register">
+                <h2 class="main-login__section-title">@lang('login.register-heading')</h2>
                 <form action="/register" method="post">
                     <label>@lang('login.login-label')</label>
                     <input type="text" name="name">
