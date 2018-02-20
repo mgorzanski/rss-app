c88200adc8336432b57341dfdfdec7e21f98418d
Mateusz <gorzanski.mateusz@gmail.com>
2018-02-20 15:39:11 +0100

    add polish translation to settings page

---
+++ resources/lang/pl/settings.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'title' =>  'Ustawienai',
+    'label-select-lang' =>  'Wybierz język',
+    'label-always-open-source-of-article' =>  'Zawsze wyświetlaj źródło artykułu',
+    'label-always-open-source-of-article-description' =>  'Po zaznaczeniu tej opcji, po kliknięciu w artykuł zawsze będzie wczytywać się bezpośredne źródło strony.',
+    'form-submit-btn'   =>  'Zapisz'
+];
+
+?>
