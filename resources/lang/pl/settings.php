2434f64bcc2b62958240ac49992452da78cb138e
Mateusz <gorzanski.mateusz@gmail.com>
2018-02-20 15:40:03 +0100

    fix typo

--- resources/lang/pl/settings.php
+++ resources/lang/pl/settings.php
@@ -4,7 +4,7 @@ return [
     'title' =>  'Ustawienai',
     'label-select-lang' =>  'Wybierz język',
     'label-always-open-source-of-article' =>  'Zawsze wyświetlaj źródło artykułu',
-    'label-always-open-source-of-article-description' =>  'Po zaznaczeniu tej opcji, po kliknięciu w artykuł zawsze będzie wczytywać się bezpośredne źródło strony.',
+    'label-always-open-source-of-article-description' =>  'Po zaznaczeniu tej opcji, po kliknięciu w artykuł zawsze będzie wczytywać się bezpośrednie źródło strony.',
     'form-submit-btn'   =>  'Zapisz'
 ];
 
