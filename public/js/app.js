e7b511e6ab9f2ab8a8fb85d52613dcde3d11c94e
Mateusz <gorzanski.mateusz@gmail.com>
2018-02-25 19:53:20 +0100

    cleanup js scripts

--- public/js/app.js
+++ public/js/app.js
@@ -76,30 +76,6 @@ module.exports = __webpack_require__(6);
 /* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-//require('./bootstrap');
-
-//window.Vue = require('vue');
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-/*
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-const app = new Vue({
-   el: '#app'
-});
-*/
-
 window.$ = window.jQuery = __webpack_require__(2);
 
 __webpack_require__(3);
